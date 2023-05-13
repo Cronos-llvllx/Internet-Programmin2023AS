6b213b91d908143565f7dd05e97738dcd002bf83
Adriel03 <99216506+Adriel03@users.noreply.github.com>
2023-05-13 15:35:46 -0600

    Update bddconfig.js

--- backend/config/bddconfig.js
+++ backend/config/bddconfig.js
@@ -1,8 +1,7 @@
 export default
 {
-    port: process.env.PORT || 1433,
     dbUser: process.env.DB_USER || "sa",
-    dbPassword: process.env.DB_PASSWORD || "balon300@",
-    dbServer: process.env.DB_SERVER || "192.168.100.181",
+    dbPassword: process.env.DB_PASSWORD || "balon300",
+    dbServer: process.env.DB_SERVER || "localhost",
     bdd:" Progra_Internet"
 }
